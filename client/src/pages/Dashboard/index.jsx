9f385b9d7e97d6555ca7068ff32ddead420d519a
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-07-08 10:28:05 -0400

    added seed data, display username on dashboard page:

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import AuthService from '../../utils/auth';
+import React from "react";
+import AuthService from "../../utils/auth";
+
+const userData = AuthService.getUser().data;
+const username = userData.username;
 
 const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <p>User: {username}</p>
       <button onClick={AuthService.logout}>Logout</button>
     </div>
-  )
+  );
 };
 
 export default Dashboard;
